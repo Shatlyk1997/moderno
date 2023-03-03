3f9b870cd45372a1c96cd49f49dfa67c959afbd3
Shatlyk1997 <shatlyk_saparkulyyev@mail.ru>
2023-03-03 09:01:29 +0300

    added product slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
-$(function () {
+$(function(){
+    $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+      });
 
-  
+    $('.products__slider-inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true
+    });
+
+    var mixer = mixitup('.products__inner-box');  
   
 });
