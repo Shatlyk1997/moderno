8c5dd7ff7b203745a3e5fb2ab43ecffbafb3e09c
Shatlyk1997 <shatlyk_saparkulyyev@mail.ru>
2023-04-05 12:33:18 +0300

    added adaptiv header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -10,9 +10,19 @@ $(function(){
     $(".product__slider-inner").slick({
     dots: true,
     arrows: false,
+    autoplay:true,
     slidesToShow: 4,
     slidesToScroll: 4
     });
+
+    $(".menu__btn").on('click', function(){
+    $(".menu__list").slideToggle();
+    });
+
+    $(".header__btn-menu").on('click', function(){
+    $(".header__box").toggleClass('active');
+    });
+
 
     var mixer = mixitup('.products__inner-box');
     
